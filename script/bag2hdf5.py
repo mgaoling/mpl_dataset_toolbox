b490c2cc96c6338ed0da6cba5a6318e3abd4fc3f
mgaoling <mgaoling@gmail.com>
2022-06-04 10:46:51 +0800

    Speed Up Bag to HDF5

--- script/bag2hdf5.py
+++ script/bag2hdf5.py
@@ -1,107 +1,84 @@
 import argparse
 import h5py
 import hdf5plugin
-import os
 import progressbar
 from prophesee_event_msgs.msg import EventArray
 import rosbag
 import numpy as np
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Convert ROS bag to hdf5 for event stream(s)')
-    parser.add_argument('dataset', type=str, help='ROS bag path')
+    parser = argparse.ArgumentParser(description='Convert ROS bag to hdf5 for event stream')
+    parser.add_argument('topic', type=str, help='ROS topic for event stream')
+    parser.add_argument('input_path', type=str, help='Input ROS bag path')
+    parser.add_argument('output_path', type=str, help='Output HDF5 file path')
     args = parser.parse_args()
     
-    left_event_topic = '/prophesee/left/events'
-    right_event_topic = '/prophesee/right/events'
-    receive_left_event = False
-    receive_right_event = False
-    event_left_x = []
-    event_left_y = []
-    event_left_p = []
-    event_left_t = []
-    event_left_ms_to_idx = []
-    event_right_x = []
-    event_right_y = []
-    event_right_p = []
-    event_right_t = []
-    event_right_ms_to_idx = []
-
-    data_path = args.dataset
-    data_path_without_extension, _ = os.path.splitext(data_path)
-    event_left_out_path = data_path_without_extension + '.left_event.hdf5'
-    event_right_out_path = data_path_without_extension + '.right_event.hdf5'
-    event_left_file = h5py.File(event_left_out_path, 'w')
-    event_right_file = h5py.File(event_right_out_path, 'w')
-
-    bag = rosbag.Bag(data_path, 'r')
-    event_left_msg_num = bag.get_message_count(left_event_topic)
-    event_left_msg_idx = 0
-    event_left_curr_idx = 0
-    event_left_ms_idx = 0
-    event_right_msg_num = bag.get_message_count(right_event_topic)
-    event_right_msg_idx = 0
-    event_right_idx = 0
-    event_right_curr_idx = 0
-    progress = progressbar.ProgressBar(maxval=(event_left_msg_num + event_right_msg_num))
-    progress.start()
-    for topic, msg, t in bag.read_messages([left_event_topic, right_event_topic]):
-        if (topic == left_event_topic):
-            if not receive_left_event:
-                receive_left_event = True
-                event_left_t_offset = msg.events[0].ts
+    event_topic = args.topic
+    input_data_path = args.input_path
+    bag = rosbag.Bag(input_data_path, 'r')
+    output_data_path = args.output_path
+    event_file = h5py.File(output_data_path, 'w')
+    
+    event_curr_idx = 0
+    event_ms_idx = 0
+    chunk_size = 100000
+    receive_event = False
+    event_x = []
+    event_y = []
+    event_p = []
+    event_t = []
+    event_ms_to_idx = []
+    bar = progressbar.ProgressBar(maxval=bag.size).start()
+    for topic, msg, t in bag.read_messages(event_topic):
+        bar.update(bag._file.tell())
+        if (topic == event_topic):
+            # Add this event into a temporary numpy holder
+            if not receive_event:
+                receive_event = True
+                event_t_offset = msg.events[0].ts
             for event in msg.events:
-                event_left_x.append(event.x)
-                event_left_y.append(event.y)
-                event_left_p.append(event.polarity)
-                event_left_t.append(int((event.ts.to_nsec() - event_left_t_offset.to_nsec()) / 1e3))
-                while (event_left_t[-1] >= 1000 * event_left_ms_idx):
-                    event_left_ms_to_idx.append(event_left_curr_idx)
-                    event_left_ms_idx += 1
-                event_left_curr_idx += 1
-            event_left_msg_idx += 1
-            
-        elif (topic == right_event_topic):
-            if not receive_right_event:
-                receive_right_event = True
-                event_right_t_offset = msg.events[0].ts
-            for event in msg.events:
-                event_right_x.append(event.x)
-                event_right_y.append(event.y)
-                event_right_p.append(event.polarity)
-                event_right_t.append(int((event.ts.to_nsec() - event_right_t_offset.to_nsec()) / 1e3))
-                while (event_right_t[-1] >= 1000 * event_right_curr_idx):
-                    event_right_ms_to_idx.append(event_right_idx)
-                    event_right_curr_idx += 1
-                event_right_idx += 1
-            event_right_msg_idx += 1
-
-        progress.update(event_left_msg_idx + event_right_msg_idx)
-    progress.finish()
-
-    event_left_x = np.array(event_left_x, dtype='u2')
-    event_left_y = np.array(event_left_y, dtype='u2')
-    event_left_p = np.array(event_left_p, dtype='u1')
-    event_left_t = np.array(event_left_t, dtype='u4')
-    event_left_ms_to_idx = np.array(event_left_ms_to_idx, dtype='u8')
-    event_right_x = np.array(event_right_x, dtype='u2')
-    event_right_y = np.array(event_right_y, dtype='u2')
-    event_right_p = np.array(event_right_p, dtype='u1')
-    event_right_t = np.array(event_right_t, dtype='u4')
-    event_right_ms_to_idx = np.array(event_right_ms_to_idx, dtype='u8')
-
-    event_left_file.create_dataset('events/x', dtype='u2', data=event_left_x, **hdf5plugin.Zstd())
-    event_left_file.create_dataset('events/y', dtype='u2', data=event_left_y, **hdf5plugin.Zstd())
-    event_left_file.create_dataset('events/p', dtype='u1', data=event_left_p, **hdf5plugin.Zstd())
-    event_left_file.create_dataset('events/t', dtype='u4', data=event_left_t, **hdf5plugin.Zstd())
-    event_left_file.create_dataset('ms_to_idx', dtype='u8', data=event_left_ms_to_idx, **hdf5plugin.Zstd())
-    event_left_file.create_dataset('t_offset', shape = (1,), dtype='i8', data=int(event_left_t_offset.to_nsec() / 1e3), **hdf5plugin.Zstd())
-    event_right_file.create_dataset('events/x', dtype='u2', data=event_right_x, **hdf5plugin.Zstd())
-    event_right_file.create_dataset('events/y', dtype='u2', data=event_right_y, **hdf5plugin.Zstd())
-    event_right_file.create_dataset('events/p', dtype='u1', data=event_right_p, **hdf5plugin.Zstd())
-    event_right_file.create_dataset('events/t', dtype='u4', data=event_right_t, **hdf5plugin.Zstd())
-    event_right_file.create_dataset('ms_to_idx', dtype='u8', data=event_right_ms_to_idx, **hdf5plugin.Zstd())
-    event_right_file.create_dataset('t_offset', shape = (1,), dtype='i8', data=int(event_right_t_offset.to_nsec() / 1e3), **hdf5plugin.Zstd())
-
-    event_left_file.close()
-    event_right_file.close()
+                event_x.append(event.x)
+                event_y.append(event.y)
+                event_p.append(event.polarity)
+                event_t.append(int((event.ts.to_nsec() - event_t_offset.to_nsec()) / 1e3))
+                while (event_t[-1] >= 1000 * event_ms_idx):
+                    event_ms_to_idx.append(event_curr_idx)
+                    event_ms_idx += 1
+                event_curr_idx += 1
+            # Flush the holder temporarily
+            if (len(event_x) >= chunk_size):
+                if ('events/t' not in event_file):
+                    event_file.create_dataset('events/x', dtype='u2', maxshape=(None, ), data=np.array(event_x, dtype='u2'), **hdf5plugin.Zstd())
+                    event_file.create_dataset('events/y', dtype='u2', maxshape=(None, ), data=np.array(event_y, dtype='u2'), **hdf5plugin.Zstd())
+                    event_file.create_dataset('events/p', dtype='u1', maxshape=(None, ), data=np.array(event_p, dtype='u1'), **hdf5plugin.Zstd())
+                    event_file.create_dataset('events/t', dtype='u4', maxshape=(None, ), data=np.array(event_t, dtype='u4'), **hdf5plugin.Zstd())
+                else:
+                    old_size = event_file['events/t'].shape[0]
+                    new_size = len(event_t) + old_size
+                    event_file['events/x'].resize((new_size, ))
+                    event_file['events/x'][old_size:] = data=np.array(event_x, dtype='u2')
+                    event_file['events/y'].resize((new_size, ))
+                    event_file['events/y'][old_size:] = data=np.array(event_y, dtype='u2')
+                    event_file['events/p'].resize((new_size, ))
+                    event_file['events/p'][old_size:] = data=np.array(event_p, dtype='u1')
+                    event_file['events/t'].resize((new_size, ))
+                    event_file['events/t'][old_size:] = data=np.array(event_t, dtype='u4')
+                event_x.clear()
+                event_y.clear()
+                event_p.clear()
+                event_t.clear()
+    # Flush the remaining data on holder
+    old_size = event_file['events/t'].shape[0]
+    new_size = len(event_t) + old_size
+    event_file['events/x'].resize((new_size, ))
+    event_file['events/x'][old_size:] = data=np.array(event_x, dtype='u2')
+    event_file['events/y'].resize((new_size, ))
+    event_file['events/y'][old_size:] = data=np.array(event_y, dtype='u2')
+    event_file['events/p'].resize((new_size, ))
+    event_file['events/p'][old_size:] = data=np.array(event_p, dtype='u1')
+    event_file['events/t'].resize((new_size, ))
+    event_file['events/t'][old_size:] = data=np.array(event_t, dtype='u4')
+    event_file.create_dataset('ms_to_idx', dtype='u8', data=np.array(event_ms_to_idx, dtype='u8'), **hdf5plugin.Zstd())
+    event_file.create_dataset('t_offset', shape = (1,), dtype='i8', data=int(event_t_offset.to_nsec() / 1e3), **hdf5plugin.Zstd())
+    bar.finish()
+    event_file.close()
